3f7eb156643900c5cee9803e9578b98a3cea1ffd
VyacheslavShtyrlin <vyacheslavshtyrlin@yandex.ru>
2022-06-12 15:01:26 +0500

    tests again

--- models/card.js
+++ models/card.js
@@ -16,10 +16,11 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: {
-    type: Array,
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     default: [],
-  },
+  }],
   createdAt: {
     type: Date,
     default: Date.now,
