e424fbbebcba5b9e323592b5d0ece970b2cb89e6
VyacheslavShtyrlin <vyacheslavshtyrlin@yandex.ru>
2022-07-16 18:24:33 +0500

    cors

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const { celebrate, Joi, errors } = require('celebrate');
-const cors = require('cors');
 const NotFound = require('./errors/notFoundError');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/errors');
@@ -17,17 +16,39 @@ const regex = require('./utils/regex');
 mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
+const allowedCors = [
+  'https://mesto.back.nomoredomains.work',
+  'http://mesto.back.nomoredomains.work',
+  'http://localhost:3001',
+];
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(cookieParser());
-
-app.use(cors({
-  credentials: true,
-}));
 
 app.use(requestLogger); // подключаем логгер запросов
 
--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,6 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(password, email)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { httpOnly: true });
       res.status(200).send({ token });
     })
     .catch(next);
