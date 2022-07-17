08dc3d79eb377d168674e0ddb3f4b845d3d21e22
VyacheslavShtyrlin <vyacheslavshtyrlin@yandex.ru>
2022-07-17 15:22:07 +0500

    review

--- app.js
+++ app.js
@@ -60,14 +60,14 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(5),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(regex),
